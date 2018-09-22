31a0448919c30ae69f864efd85d15a9b82c93483
Vitaliy Siomochkin <v.siomochkin@gmail.com>
2018-09-22 14:40:36 +0300

    Initial commit

---
+++ js/products/categories/bags.js
@@ -0,0 +1,97 @@
+/* jshint undef: true, unused: true */
+/* global define: false */
+define([], function(){
+	return {
+		name: "Bags",
+		oneRow: false,
+		imagesPath: "http://placehold.it/",
+		addCodePath: false,
+		items: [
+			{
+				name: "Eastpak Padded Pak'R Backpack",
+				price: {
+					current: "68.00"
+				},
+				code: "N8101",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates odit vitae repellat doloremque dolorem, numquam.",
+				thumb: "290x370",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Cayler And Sons Milano Backpack",
+				price: {
+					current: "53.00"
+				},
+				code: "N8102",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, animi quam obcaecati.",
+				thumb: "290x370",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Cayler And Sons Milano Backpack",
+				price: {
+					current: "38.00",
+					old: "48.00"
+				},
+				code: "N8103",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "290x370",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Reclaimed Vintage African Bum Bag",
+				price: {
+					current: "32.00",
+					old: "35.30"
+				},
+				code: "N8104",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "290x370",
+				gallery: ["460x590", "460x590", "460x590"],
+			},
+			{
+				name: "Mi-Pac Floral Tapestry Backpack",
+				price: {
+					current: "16.50",
+					old: "48.00"
+				},
+				code: "N8105",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "290x370",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Herschel Woodlands Backpack",
+				price: {
+					current: "78.00"
+				},
+				code: "N8106",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "290x370",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "Spiral Floral Backpack",
+				price: {
+					current: "20.00",
+					old: "25.00"
+				},
+				code: "N8107",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "290x370",
+				gallery: ["460x590", "460x590", "460x590"]
+			},
+			{
+				name: "River Island Backpack",
+				price: {
+					current: "55.30",
+					old: "60.00"
+				},
+				code: "N8108",
+				details: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+				thumb: "290x370",
+				gallery: ["460x590", "460x590", "460x590"]
+			}
+		]
+	};
+});
